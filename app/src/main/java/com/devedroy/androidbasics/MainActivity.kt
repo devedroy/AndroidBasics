7d565c39d34bf0f9234d61af8e600980c9dccd4d
devedroy <devedroy.dr@gmail.com>
2022-12-22 00:11:00 +0530

    Basic

--- app/src/main/java/com/devedroy/androidbasics/MainActivity.kt
+++ app/src/main/java/com/devedroy/androidbasics/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.devedroy.androidbasics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    //Activity Running
+
+    override fun onPause() {
+        super.onPause()
+
+        //used to save data before app is killed by the system
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
